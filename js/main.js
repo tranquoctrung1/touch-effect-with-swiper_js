084e77b38c4d78b7949377f027398e1400ae2de1
Trung <tranquoctrung.12k9@gmail.com>
2019-07-26 16:48:49 +0700

    add new

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+  });
